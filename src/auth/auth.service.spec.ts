ff22bcf3367c0200523bd7e55fe0cd31f6ed8242
Sebastian Bejga <sebastian@bejga.de>
2020-05-07 12:38:28 +0200

    local users file passport strategy

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User, UsersService } from '../users/users.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  let mockUsersService = {
+    findOne: (username): Promise<User> => {
+      return Promise.resolve({
+        userId: "0",
+        username: "test",
+        hashedPassword: "25bf8e1a2393f1108d37029b3df5593236c755742ec93465bbafa9b290bddcf6",
+      })
+    }
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: mockUsersService}
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { createHash } from "crypto"
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService) {}
+
+    // TODO: use bcrypt
+    public hash(str: string) {
+        return createHash("sha256").update(str).digest("hex");
+    }
+
+    async validateUser(username: string, pass: string): Promise<any> {
+        const hashedPassword = this.hash(pass);
+        const user = await this.usersService.findOne(username);
+        if (user && user.hashedPassword === hashedPassword) {
+            const { hashedPassword, ...result } = user;
+            return result;
+        }
+        return null;
+    }
+}
