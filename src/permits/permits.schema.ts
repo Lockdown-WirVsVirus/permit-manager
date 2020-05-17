ad54bec54c47f4e7c6ca2f8a7289bfe4b02c1c0f
Iranox <tgwieprich@gmail.com>
2020-05-17 17:50:48 +0200

    Update src/permits/permits.schema.ts
    Co-authored-by: Sebastian Bejga <sebastian@bejga.de>

--- src/permits/permits.schema.ts
+++ src/permits/permits.schema.ts
@@ -6,7 +6,10 @@ export const PERMIT_MODEL_NAME = 'Permit';
 export interface PermitModel extends PermitCode, mongoose.Document {}
 
 export const permitSchema = new mongoose.Schema(
-    { code: String, reasonAbbrevation: String },
+    { 
+        code: { type: String, unique: true }, 
+        reasonAbbrevation: String 
+    },
     {
         toObject: {
             virtuals: true,
