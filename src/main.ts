4c13d9bfd092d0cb3f44c2a313ca00b58dbdc010
Sebastian Bejga <sebastian@bejga.de>
2020-05-26 02:15:50 +0200

    helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 3010;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // enable http web security
+  app.use(helmet());
+
+  // enable cors
+  app.enableCors();
+
   await app.listen(port);
   console.log("listing on port " + port);
 }
